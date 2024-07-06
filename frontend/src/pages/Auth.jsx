97785d6ddf2843ba6041c9315fffb3f316b7f315
Ayush <aayushgupta023@gmail.com>
2024-07-06 20:23:22 +0530

    Error Toast Added

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styles from "./Auth.module.css";
 import {
   createUserWithEmailAndPassword,
@@ -6,7 +6,10 @@ import {
 } from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import Error from "../components/Error";
 export default function Auth() {
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState("");
   const email = useRef(null);
   const password = useRef(null);
   const navigate = useNavigate();
@@ -18,11 +21,17 @@ export default function Auth() {
         email.current.value,
         password.current.value
       );
+      setIsError(false);
       const token = user.user.accessToken;
       console.log(user.user, user.user.accessToken);
       localStorage.setItem("token", token);
       navigate("/home");
     } catch (err) {
+      setIsError(true);
+      setError(err?.code);
+      setTimeout(() => {
+        setIsError(false);
+      }, 2000);
       console.log("Error");
     }
   }
@@ -35,55 +44,68 @@ export default function Auth() {
         email.current.value,
         password.current.value
       );
+      setIsError(false);
       const token = user.user.accessToken;
       console.log(user.user, user.user.accessToken);
       localStorage.setItem("token", token);
       navigate("/home");
     } catch (err) {
+      setIsError(true);
+      setError(err?.code);
+      setTimeout(() => {
+        setIsError(false);
+      }, 2000);
       console.log("Error");
     }
   }
   return (
-    <div className={styles.auth}>
-      <div className={styles.container}>
-        <div className={styles.box}>
-          <label htmlFor="email" className={styles.label}>
-            Email:
-          </label>
-          <input
-            className={styles.input}
-            type="email"
-            id="email"
-            autoComplete="off"
-            placeholder="xyz@gmail.com"
-            ref={email}
-          />
-        </div>
-        <div className={styles.box}>
-          <label className={styles.label} htmlFor="password">
-            Password:
-          </label>
-          <input
-            className={styles.input}
-            type="password"
-            id="password"
-            minLength={8}
-            ref={password}
-          />
-        </div>
-        <div className={styles.btns}>
-          <button onClick={onLoginHandler} className={styles.btn} type="submit">
-            Login
-          </button>
-          <button
-            onClick={onSignupHandler}
-            className={styles.btn}
-            type="submit"
-          >
-            Signup
-          </button>
+    <>
+      {isError && <Error>{error}</Error>}
+      <div className={styles.auth}>
+        <div className={styles.container}>
+          <div className={styles.box}>
+            <label htmlFor="email" className={styles.label}>
+              Email:
+            </label>
+            <input
+              className={styles.input}
+              type="email"
+              id="email"
+              autoComplete="off"
+              placeholder="xyz@gmail.com"
+              ref={email}
+            />
+          </div>
+          <div className={styles.box}>
+            <label className={styles.label} htmlFor="password">
+              Password:
+            </label>
+            <input
+              className={styles.input}
+              type="password"
+              id="password"
+              minLength={8}
+              ref={password}
+            />
+          </div>
+          <div className={styles.btns}>
+            <button
+              onClick={onLoginHandler}
+              className={styles.btn}
+              type="submit"
+            >
+              Login
+            </button>
+            <button
+              onClick={onSignupHandler}
+              className={styles.btn}
+              type="submit"
+            >
+              Signup
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
